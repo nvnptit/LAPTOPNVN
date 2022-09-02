5d56c3c87ef68998619208b29859ffc5660a5cba
nvnptit <nvnptit@gmail.com>
2022-09-02 13:50:35 +0700

    [FEAT] update

--- LAPTOPNVN/Modules/Main/ManagerAdmin/Employee/Cell/EmployeeTableViewCell.swift
+++ LAPTOPNVN/Modules/Main/ManagerAdmin/Employee/Cell/EmployeeTableViewCell.swift
@@ -18,7 +18,6 @@ class EmployeeTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
